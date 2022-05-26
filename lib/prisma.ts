b19bce01bc9227f5b2f1bd4f4260ac6f2ae2bc5a
Martin Ždila <m.zdila@gmail.com>
2022-05-26 08:14:48 +0200

    fixed many db connections

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  // log: ["query", "info", "warn", "error"],
-});
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    // log: ['query'],
+    // log: ["query", "info", "warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
