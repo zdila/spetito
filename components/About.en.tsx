226bd9274135c9b3e18a1e9486c0140bbe4f7644
Martin Ždila <m.zdila@gmail.com>
2022-06-10 20:11:28 +0200

    google translated "about" (refs #18)

---
+++ components/About.en.tsx
@@ -0,0 +1,94 @@
+import { Box, Typography } from "@mui/material";
+
+export default function AboutEn() {
+  return (
+    <Box
+      sx={{
+        li: {
+          fontStyle: "italic",
+        },
+      }}
+    >
+      <h2>What is the mission of Spetito?</h2>
+
+      <p>
+        <i>
+          Note that the text on this page is currently just Google-translated.
+        </i>
+      </p>
+
+      <p>
+        Spetito (<b>Spe</b>nd<b> ti</b>me<b> to</b>gether) is simple social
+        network for giving non-binding offers to your friends, and vice versa
+        parties, for a comprehensive overview of all offers from your friends.
+        Offers they might look like this:
+      </p>
+
+      <ul>
+        <li>
+          I&apos;m going to bike the Košice singletrails. I welcome the biker
+          company.
+        </li>
+
+        <li>
+          I will be at the cottage in the evening, I will be pleased with the
+          visit. The beer is chilled. Before please contact me.
+        </li>
+
+        <li>
+          This weekend I am planning a trip from Štrbské pleso to Kriváň.
+          Companions, report. The weather is supposed to be per unit.
+        </li>
+
+        <li>
+          I will go from Košice to Bratislava on Friday and I will return on
+          Sunday. if someone needs some transportation, let me write.
+        </li>
+
+        <li>
+          I bake a great raw cake and I hope I won&apos;t eat it alone at home.
+        </li>
+
+        <li> I teach math 😜 </li>
+      </ul>
+
+      <p>
+        Sharing groups are also planned in the near future similar interests,
+        ideally in the same location, for example:
+        <i> Košice dogs </i>, <i> Prešov tourists </i>,
+        <i> The lonely souls of Nitra </i> etc.
+      </p>
+
+      <h2> Why not Facebook? </h2>
+
+      <p>
+        Although Facebook contains events and it is also possible to post offers
+        in messages, but it contains a lot of other things that make it
+        impossible search for all relevant offers. The events on Facebook
+        themselves are understood as invitations, while at Spetite they are
+        (non-binding) offers, in the form of similar advertisement, but only
+        within the circle of friends. Few people will create a Facebook event
+        that makes you go a little jogging tonight and would love to he had some
+        company in the process. Spetito is ideal for such offers.
+      </p>
+
+      <p>
+        In addition, Facebook is full of irrelevant sponsored posts and
+        advertising. You&apos;ll also never find some friends on Facebook,
+        because not everyone &quot;loves him&quot;.
+      </p>
+
+      <h2> Why don&apos;t offers include comments and likes? </h2>
+
+      <p>
+        Spetito is only for posting and browsing friends&apos; offers. if
+        someone offers an offer, then contacts his friend another by road, for
+        example by telephone, or by a communication application such as theirs
+        together. A platoon is also planned for Spetite in the future. The idea
+        is that communication should only be between the bidder and the
+        interested party; not among a group of stakeholders who may not even
+        know each other.
+      </p>
+    </Box>
+  );
+}
--- components/About.tsx
+++ components/About.tsx
@@ -1,88 +1,14 @@
-import { Box, Typography } from "@mui/material";
-import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
+import { lazy, Suspense } from "react";
 
 export function About() {
-  const { t } = useTranslation("common");
+  const { locale } = useRouter();
+
+  const AboutLocalized = lazy(() => import("./About." + locale));
 
   return (
-    <Box
-      sx={{
-        li: {
-          fontStyle: "italic",
-        },
-      }}
-    >
-      <h2>Aká je misia Spetita?</h2>
-
-      <p>
-        Spetito (<b>Spe</b>nd <b>ti</b>me <b>to</b>gether) je jednoduchá
-        sociálna sieť pre dávanie nezáväzných ponúk vaším priateľom, a z opačnej
-        strany, na ucelený prehľad všetkých ponúk od vaších priateľov. Ponuky
-        môžu vyzerať napríklad takto:
-      </p>
-
-      <ul>
-        <li>
-          Idem sa bikovať na košické singletraily. Uvítam bikerskú spoločnosť.
-        </li>
-
-        <li>
-          Večer budem na chate, návšteva ma poteší. Pivko je vychladené. Predtým
-          sa mi prosím ozvite.
-        </li>
-
-        <li>
-          Tento víkend plánujem výlet zo Štrbského plesa na Kriváň. Parťáci,
-          hláste sa. Počasie má byť na jednotku.
-        </li>
-
-        <li>
-          V piatok pôjdem z Košíc do Bratislavy a v nedeľu sa budem vraciať. Ak
-          niekto potrebuje nejaký prevoz, nech mi píše.
-        </li>
-
-        <li>
-          Pečiem parádny raw koláčik a dúfam, že ho sám doma jesť nebudem.
-        </li>
-
-        <li>Doučím matematiku 😜</li>
-      </ul>
-
-      <p>
-        V blízkej budúcnosti sú naplánované aj skupiny - ľudia zdieľajúci
-        podobné záujmy, ideálne v rovnakej lokalite, napríklad:{" "}
-        <i>Košickí psíčkari</i>, <i>Prešovskí turisti</i>,{" "}
-        <i>Osamelé duše Nitry</i> a pod.
-      </p>
-
-      <h2>Prečo nie Facebook?</h2>
-
-      <p>
-        Facebook síce obsahuje udalosti a tiež je v ňom možné písať ponuky do
-        správ, ale obsahuje ešte plno iných vecí, ktoré znemožňujú jednoduché
-        vyhľadanie všetkých relevantných ponúk. Samotné udalosti na Facebooku sú
-        chápané ako pozvánky, pričom na Spetite sú to (nezáväzné) ponuky, formou
-        podobnou inzerátom, no iba v rámci kruhu priateľov. Málokto asi bude
-        vytvárať na Facebooku udalosť, že si ide večer trochu zabehať a rád by
-        mal pri tom nejakú spoločnosť. Spetito je ideálny (aj) na takéto ponuky.
-      </p>
-
-      <p>
-        Okrem toho, na Facebooku je plno irelevantných sponzorovaných príspevkov
-        a reklám. Na Facebooku tiež nikdy nenájdete niektorých priateľov,
-        pretože nie každý ho &quot;má v láske&quot;.
-      </p>
-
-      <h2>Prečo ponuky neobsahujú komentáre a &quot;páčiky&quot;?</h2>
-
-      <p>
-        Spetito slúži iba na zverejňovanie a prehliadanie ponúk priateľov. Ak
-        niekoho nejaká ponuka osloví, následne kontaktuje svojho priateľa inou
-        cestou, napríklad telefonicky, alebo komunikačnou aplikáciou, akú majú
-        spolu zaužívanu. Na Spetite je do budúcna plánovaný aj čet. Myšlienka
-        je, že komunikácia má byť iba medzi ponúkajucim a zainteresovaným; nie
-        medzi skupinou zainteresovaných, ktorí sa ani nemusia poznať.
-      </p>
-    </Box>
+    <Suspense>
+      <AboutLocalized />
+    </Suspense>
   );
 }
